ed025228b86bf9d8c61bcada65cb92764fc41643
louis.hameau <louis.hameau@campus-centre.fr>
2025-03-04 16:03:26 +0100

    Premier Commit

--- poomvc/views/ajouter.php
+++ poomvc/views/ajouter.php
@@ -4,8 +4,8 @@ include 'header.php';
 <h2>Ajouter un Produit</h2>
 <form method="post">
     Nom: <input type="text" name="nom" required><br>
-    Prix: <input type="number" step="0.01" name="prix" required><br>
-    Quantité: <input type="number" name="quantite" required><br>
+    Prix : <input type="number" step="0.01" name="prix" required><br>
+    Quantité : <input type="number" name="quantite" required><br>
     <input type="submit" value="Ajouter" class="submit">
 </form>
 <a href="index.php?action=liste"> ◄ Retour</a>
